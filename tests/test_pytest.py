cc279f4ab3fe59254a89f21f7c7d7bad000b73ff
nasstulina <n.mishustina44@gmail.com>
2025-11-29 17:20:40 +0700

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,13 @@
+def test_first_try():
+    print('Hello!python')
+
+
+def test_assert_negative_case():  # Новый тест, которые проверяет негативный кейс
+    assert (2 + 2) == 5  # Тут должна быть ошибка
+
+
+def test_assert_positive_case():  # Новый тест, которые проверяет положительный кейс
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
+
+
+
