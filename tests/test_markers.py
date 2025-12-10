dc1c35d61327d3a7370632b102454c51c27585dd
nasstulina <n.mishustina44@gmail.com>
2025-12-10 15:16:24 +0700

    Практика работы с pytest маркировками

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -1,5 +1,14 @@
 import pytest
 
+@pytest.mark.registration
+def test_user_registration():
+    pass
+
 @pytest.mark.smoke
-def test_smoke_case():
-    ...
+def test_user_login():
+    pass
+
+@pytest.mark.registration
+@pytest.mark.regression
+def test_password_reset():
+    pass
