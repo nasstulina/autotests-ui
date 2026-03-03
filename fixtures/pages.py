ff08053d186059368a4ed18aee5e3d3a6b446875
nasstulina <n.mishustina44@gmail.com>
2026-03-03 21:38:09 +0700

    Практика работы с Page Object

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,8 +1,20 @@
 import pytest
 from playwright.sync_api import Page
 
+from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(chromium_page)
+
+@pytest.fixture
+def registration_page(chromium_page: Page) -> RegistrationPage:
+    return RegistrationPage(chromium_page)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(chromium_page)
