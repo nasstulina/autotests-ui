a05bfc066738a258cade7ad93d67d45ccdca5e56
nasstulina <n.mishustina44@gmail.com>
2026-03-05 17:12:09 +0700

    Практика в реализации PageObject

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -7,7 +7,44 @@ class DashboardPage(BasePage):
         super().__init__(page)
         self.dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
 
+        self.student_title = page.get_by_test_id('students-widget-title-text')
+        self.student_chart = page.get_by_test_id('students-bar-chart')
+
+        self.activities_title = page.get_by_test_id('activities-widget-title-text')
+        self.activities_chart = page.get_by_test_id('activities-line-chart')
+
+        self.courses_title = page.get_by_test_id('courses-widget-title-text')
+        self.courses_chart = page.get_by_test_id('courses-pie-chart')
+
+        self.scores_title = page.get_by_test_id('scores-widget-title-text')
+        self.scores_chart = page.get_by_test_id('scores-scatter-chart')
+
 
     def check_dashboard_title_visible(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
+
+
+    def check_student_chart_visible(self):
+        expect(self.student_title).to_be_visible()
+        expect(self.student_title).to_have_text('Students')
+        expect(self.student_chart).to_be_visible()
+
+
+    def check_activities_chart_visible(self):
+        expect(self.activities_title).to_be_visible()
+        expect(self.activities_title).to_have_text('Activities')
+        expect(self.activities_chart).to_be_visible()
+
+    def check_courses_chart_visible(self):
+        expect(self.courses_title).to_be_visible()
+        expect(self.courses_title).to_have_text('Courses')
+        expect(self.courses_chart).to_be_visible()
+
+    def check_scores_chart_visible(self):
+        expect(self.scores_title).to_be_visible()
+        expect(self.scores_title).to_have_text('Scores')
+        expect(self.scores_chart).to_be_visible()
+
+
+
